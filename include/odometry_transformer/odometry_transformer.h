e8ddea3000b172c330db736ceb6b717cab0b5bc7
Rik Bähnemann <brik@ethz.ch>
2022-03-29 16:33:58 +0200

    Subscriber.

--- include/odometry_transformer/odometry_transformer.h
+++ include/odometry_transformer/odometry_transformer.h
@@ -3,6 +3,7 @@
 
 #include <string>
 
+#include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
 
 namespace odometry_transformer {
@@ -11,13 +12,20 @@ public:
   OdometryTransformer(const ros::NodeHandle &nh,
                       const ros::NodeHandle &nh_private);
   void getRosParameters();
+  void subscribeToRosTopics();
 
 private:
-  std::string parent_frame_ = "";
-  std::string child_frame_ = "";
+  void receiveOdometry(const nav_msgs::OdometryConstPtr &msg);
+
+  std::string source_frame_ = "";
+  std::string target_frame_ = "";
+  int queue_size_ = 1;
 
   ros::NodeHandle nh_;
   ros::NodeHandle nh_private_;
+
+  ros::Subscriber odometry_sub_;
+  ros::Publisher odometry_pub_;
 };
 } // namespace odometry_transformer
 
--- src/odometry_transformer.cc
+++ src/odometry_transformer.cc
@@ -5,14 +5,32 @@ OdometryTransformer::OdometryTransformer(const ros::NodeHandle &nh,
                                          const ros::NodeHandle &nh_private)
     : nh_(nh), nh_private_(nh_private) {
   getRosParameters();
+  subscribeToRosTopics();
 }
 
 void OdometryTransformer::getRosParameters() {
   const std::string log_param_error = "Did not find ROS parameter: ";
-  ROS_WARN_STREAM_COND(!nh_private_.getParam("parent_frame", parent_frame_),
-                       log_param_error.c_str() << "parent_frame");
-  ROS_WARN_STREAM_COND(!nh_private_.getParam("child_frame", child_frame_),
-                       log_param_error.c_str() << "child_frame");
+
+  ROS_WARN_STREAM_COND(!nh_private_.getParam("source_frame", source_frame_),
+                       log_param_error.c_str() << "source_frame");
+  ROS_WARN_STREAM_COND(!nh_private_.getParam("target_frame", target_frame_),
+                       log_param_error.c_str() << "target_frame");
+  ROS_INFO("Transforming odometry from %s frame to %s frame.",
+           source_frame_.c_str(), target_frame_.c_str());
+
+  nh_private_.getParam("queue_size", queue_size_);
+  ROS_INFO("Odometry queue size: %d", queue_size_);
+}
+
+void OdometryTransformer::subscribeToRosTopics() {
+  odometry_sub_ = nh_.subscribe("source_odometry", queue_size_,
+                                &OdometryTransformer::receiveOdometry, this);
+  ROS_INFO("Subscribed to %s", odometry_sub_.getTopic().c_str());
+}
+
+void OdometryTransformer::receiveOdometry(
+    const nav_msgs::OdometryConstPtr &msg) {
+  ROS_INFO_ONCE("Received first odometry message.");
 }
 
 } // namespace odometry_transformer
